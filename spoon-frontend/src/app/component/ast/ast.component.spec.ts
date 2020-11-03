e3ede6099f626c4c16b935ce2374451621bf2974
Guill Robin <robin.guill@insa-rennes.fr>
2020-11-03 09:40:56 +0100

    Ast Component Unit Testing

--- spoon-frontend/src/app/component/ast/ast.component.spec.ts
+++ spoon-frontend/src/app/component/ast/ast.component.spec.ts
@@ -1,11 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AstComponent } from './ast.component';
+import {AstComponent, ASTNode} from './ast.component';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {NestedTreeControl} from '@angular/cdk/tree';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
 
 describe('AstComponent', () => {
   let component: AstComponent;
   let fixture: ComponentFixture<AstComponent>;
+
+  const testData: ASTNode[] = [
+    {
+      label: 'Fruit',
+      children: [
+        {label: 'Apple'},
+        {label: 'Banana'},
+        {label: 'Fruit loops'},
+      ]
+    }, {
+      label: 'Vegetables',
+      children: [
+        {
+          label: 'Green',
+          children: [
+            {label: 'Broccoli'},
+            {label: 'Brussels sprouts'},
+          ]
+        }, {
+          label: 'Orange',
+          children: [
+            {label: 'Pumpkins'},
+            {label: 'Carrots'},
+          ]
+        },
+      ]
+    },
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,4 +55,28 @@ describe('AstComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should have treeControl', () => {
+    expect(component.treeControl).toBeInstanceOf(NestedTreeControl);
+  });
+
+  it('should have a dataSource', () => {
+    expect(component.dataSource).toBeInstanceOf(MatTreeNestedDataSource);
+    expect(component.dataSource.data).toEqual(testData);
+  });
+
+  it('should change view after changes', () => {
+    spyOn(component, 'ngAfterViewInit');
+    component.dataSource.data = [
+      {
+        label: 'Foo',
+        children: []
+      },
+      {
+        label: 'Bar',
+        children: []
+      }
+    ];
+    // TODO
+    expect(component.ngAfterViewInit).toHaveBeenCalled();
+  });
 });
