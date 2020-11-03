0360cc5544e3d5cd791e8b9fa7953b7ab1e3adf6
Guill Robin <robin.guill@insa-rennes.fr>
2020-11-03 09:41:42 +0100

    Update code REST Testing configuration

--- spoon-frontend/src/app/command/update-code.spec.ts
+++ spoon-frontend/src/app/command/update-code.spec.ts
@@ -1,7 +1,39 @@
 import { UpdateCode } from './update-code';
+import {async, TestBed} from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import createSpyObj = jasmine.createSpyObj;
 
 describe('UpdateCode', () => {
-  // it('should create an instance', () => {
-  //   expect(new UpdateCode()).toBeTruthy();
-  // });
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        UpdateCode,
+        HttpTestingController
+      ],
+    }).compileComponents();
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    }));
+
+  it('spec name', () => {
+    const data = {
+      label: 'foo',
+      children: []
+    };
+    // TODO
+    // const cmd = createSpyObj<UpdateCode>();
+    // cmd.doIt();
+    const req = httpTestingController.expectOne('spoon/ast'); // on check la route
+    req.flush(data); // on mock les données retournées
+
+    expect(req.request.method).toEqual('POST'); // on check le verbe
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
 });
