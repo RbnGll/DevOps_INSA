89d9f1b23bc48862b4799f69200b92ae53431d86
Guill Robin <robin.guill@insa-rennes.fr>
2020-11-06 12:45:24 +0100

    REST tests

--- spoon-frontend/src/app/command/update-code.spec.ts
+++ spoon-frontend/src/app/command/update-code.spec.ts
@@ -1,31 +1,70 @@
 import { UpdateCode } from './update-code';
 import {async, TestBed} from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import createSpyObj = jasmine.createSpyObj;
+import { HttpClient} from '@angular/common/http';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
+import {ASTNode} from '../component/ast/ast.component';
+import {CommandImpl} from 'interacto';
 
 describe('UpdateCode', () => {
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
+  let dataSource: MatTreeNestedDataSource<ASTNode>;
+  let update: UpdateCode;
+  const testData: ASTNode[] = [
+    {
+      label: 'Fruit',
+      children: [
+        {label: 'Apple'},
+        {label: 'Banana'},
+        {label: 'Fruit loops'},
+      ]
+    }, {
+      label: 'Vegetables',
+      children: [
+        {
+          label: 'Green',
+          children: [
+            {label: 'Broccoli'},
+            {label: 'Brussels sprouts'},
+          ]
+        }, {
+          label: 'Orange',
+          children: [
+            {label: 'Pumpkins'},
+            {label: 'Carrots'},
+          ]
+        },
+      ]
+    },
+  ];
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        UpdateCode,
-        HttpTestingController
-      ],
+      imports: [
+        HttpClientTestingModule
+      ]
     }).compileComponents();
+  }));
+
+  beforeEach(() => {
     httpClient = TestBed.inject(HttpClient);
     httpTestingController = TestBed.inject(HttpTestingController);
-    }));
+    dataSource = new MatTreeNestedDataSource();
+    dataSource.data = testData;
+    });
 
-  it('spec name', () => {
+  it('should create UpdateCode instance of CommandImpl', () => {
+    update = new UpdateCode(httpClient, dataSource);
+    expect(update).toBeInstanceOf(CommandImpl);
+  });
+
+  it('spec REST tests', () => {
+    update = new UpdateCode(httpClient, dataSource);
     const data = {
       label: 'foo',
       children: []
     };
-    // TODO
-    // const cmd = createSpyObj<UpdateCode>();
-    // cmd.doIt();
+    update.doIt();
     const req = httpTestingController.expectOne('spoon/ast'); // on check la route
     req.flush(data); // on mock les données retournées
 
